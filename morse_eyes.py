4d280c531409c5427efcb767a94909ce9493a993
Ayman Mohammed <124022417+Ayman047@users.noreply.github.com>
2025-05-26 18:05:32 +0530

    Update morse_eyes.py

--- morse_eyes.py
+++ morse_eyes.py
@@ -9,39 +9,27 @@ import numpy as np
 from morse_log import log
 
 class Detectmorse():
-
-    # Constructor...
     def __init__(self):
         self.flag = 0
         self.openEye = 0
-        self.str = ''
+        self.current_morse_seq = []
         self.finalString = []
-        global L
-        self.L = []
-        self.closed = False
-        self.timer = 0
-        self.final = ''
         self.pts = deque(maxlen=512)
-        self.thresh = 0.25
-        self.dot = 10
-        self.dash = 40
+        self.thresh = 0.26  # EAR threshold (adjusted slightly)
         self.detect = dlib.get_frontal_face_detector()
-        self.predict = dlib.shape_predictor(
-            "shape_predictor_68_face_landmarks.dat")  # Dat file is the crux of the code
+        self.predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
         (self.lStart, self.lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
         (self.rStart, self.rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-    def eye_aspect_ratio(self,eye):
+    def eye_aspect_ratio(self, eye):
         A = distance.euclidean(eye[1], eye[5])
         B = distance.euclidean(eye[2], eye[4])
         C = distance.euclidean(eye[0], eye[3])
-        self.ear = (A + B) / (2.0 * C)
-        return self.ear
+        ear = (A + B) / (2.0 * C)
+        return ear
 
-
-    def calculate(self,frame):
-
+    def calculate(self, frame):
         decoded = cv2.imdecode(np.frombuffer(frame, np.uint8), -1)
         frame = imutils.resize(decoded, width=640)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -49,65 +37,72 @@ class Detectmorse():
         subjects = self.detect(gray, 0)
         for subject in subjects:
             shape = self.predict(gray, subject)
-            shape = face_utils.shape_to_np(shape)  # converting to NumPy Array
+            shape = face_utils.shape_to_np(shape)
+
             leftEye = shape[self.lStart:self.lEnd]
             rightEye = shape[self.rStart:self.rEnd]
+
             leftEAR = self.eye_aspect_ratio(leftEye)
             rightEAR = self.eye_aspect_ratio(rightEye)
             ear = (leftEAR + rightEAR) / 2.0
+
             leftEyeHull = cv2.convexHull(leftEye)
             rightEyeHull = cv2.convexHull(rightEye)
+
             cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
             cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-            if ear < self.thresh:  # closed eyes
+
+            if ear < self.thresh:  # eyes closed
                 self.flag += 1
-                self.pts.appendleft(self.flag)
                 self.openEye = 0
-            else:
-                self.openEye += 1
-                self.flag = 0
                 self.pts.appendleft(self.flag)
-            for i in range(1, len(self.pts)):
-                if self.pts[i] > self.pts[i - 1]:
-                    # print(pts[i - 1], pts[i])
-                    if self.pts[i] > 30 and self.pts[i] < 60:
-                        print("Eyes have been closed for 50 frames! - Print '-'")
-                        log("Eyes have been closed for 50 frames!")
-                        self.L.append("-")
-                        self.pts = deque(maxlen=512)
-                        break
-                    elif self.pts[i] > 15 and self.pts[i] < 30:
-                        print("Eyes have been closed for 20 frames!")
-                        log("Eyes have been closed for 20 frames! - Print '.'")
-                        self.L.append(".")
-                        self.pts = deque(maxlen=512)
-                        break
+            else:  # eyes open
+                if self.flag != 0:
+                    blink_length = self.flag
+                    self._interpret_blink(blink_length)
+                self.flag = 0
+                self.openEye += 1
+                self.pts.appendleft(self.flag)
 
-                    elif self.pts[i] > 60:
-                        print("Eyes have been closed for 60 frames!")
-                        log("Eyes have been closed for 60 frames! - Remove morse character")
-                        self.L.pop()
-                        self.pts = deque(maxlen=512)
-                        break
+        # If open eyes for certain frames, decode Morse
+        if self.openEye > 25 and self.current_morse_seq:
+            morse_code = ''.join(self.current_morse_seq)
+            decoded_char = convertMorseToText(morse_code)
+            if decoded_char:
+                self.finalString.append(decoded_char)
+                log(f"Decoded character: {decoded_char}")
+            else:
+                log(f"Failed to decode: {morse_code}")
+            self.current_morse_seq = []
 
-        if (self.L != []):
+        # Draw final decoded text
+        final_text = ''.join(self.finalString)
+        morse_text = ''.join(self.current_morse_seq)
 
-            print(self.L)
-        if self.openEye > 60:
-            if (self.L != []):
-                print(self.L)
-            self.str = convertMorseToText(''.join(self.L))
+        cv2.putText(frame, "Morse: " + morse_text, (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
 
-            if self.str != None:
-                print(self.str)
-                self.finalString.append(self.str)
-                self.final = ''.join(self.finalString)
-            if self.str == None:
-                self.L = []
-            self.L = []
-        cv2.putText(frame, "Predicted :  " + self.final, (10, 470),
-                    cv2.FONT_HERSHEY_DUPLEX, 0.7, (52, 152, 219), 2)
+        cv2.putText(frame, "Decoded: " + final_text, (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (52, 152, 219), 2)
+
         ret, png = cv2.imencode('.png', frame)
-        return png, self.L
+        return png, self.current_morse_seq
 
+    def _interpret_blink(self, frames_closed):
+        if 4 <= frames_closed < 8:
+            self.current_morse_seq.append('.')
+            log(f"Dot detected ({frames_closed} frames)")
+            print(f"Dot detected ({frames_closed} frames)")
+        elif 8 <= frames_closed < 15:
+            self.current_morse_seq.append('-')
+            log(f"Dash detected ({frames_closed} frames)")
+            print(f"Dash detected ({frames_closed} frames)")
+        elif frames_closed >= 15:
+            if self.current_morse_seq:
+                removed = self.current_morse_seq.pop()
+                log(f"Long blink detected, removed last Morse character: {removed}")
+                print(f"Long blink detected, removed last Morse character: {removed}")
 
+    @property
+    def final(self):
+        return ''.join(self.finalString)
