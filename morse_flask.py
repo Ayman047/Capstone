272ea397ca063c3428cafcee2f3d09e15ebfebdc
Ayman Mohammed <124022417+Ayman047@users.noreply.github.com>
2025-05-26 18:02:43 +0530

    Update morse_flask.py

--- morse_flask.py
+++ morse_flask.py
@@ -9,43 +9,32 @@ morse = Detectmorse()
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
-
 
 def gen(camera):
     while True:
         frame = camera.get_frame()
         png, L = morse.calculate(frame)
-        streambyte()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + png.tobytes() + b'\r\n')
-
-
-
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-
 @app.route('/yield')
 def streambyte():
-    L = morse.L
-    final = morse.final
-    #print("raghav",L)
-
+    L = morse.current_morse_seq
+    final = ''.join(morse.finalString)
     b = (''.join(L)).encode('utf-8')
-    c = (final).encode('utf-8')
+    c = final.encode('utf-8')
     log(b)
     log(c)
     def events():
-        yield "data: %s %s\n\n" % (b,c)
-        time.sleep(1)  # an artificial delay
+        yield f"data: {b.decode()} {c.decode()}\n\n"
+        time.sleep(1)
     return Response(events(), content_type='text/event-stream')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threaded=True)
